740afbd35c209c8dd3e5f77e2ab9a2a1a18c8d40
Alejandro Cortez <ziker@live.com.mx>
2021-01-26 18:21:48 -0800

    Modified some information in README.md

--- src/lib/apollo-client.js
+++ src/lib/apollo-client.js
@@ -2,7 +2,7 @@ import { ApolloClient, HttpLink, InMemoryCache, split } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const BASE_URI = 'https://api.github.com/graphql';
-const TOKEN = 'fb6ff1222ad200635d34f31e0394d81edae427f1';
+const TOKEN = '1cda68aa60fa3ee0831b1cb709e1102d5b237238';
 
 const httpLink = new HttpLink({
   uri: BASE_URI,
